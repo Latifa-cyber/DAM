45016b6aa5a68dc0dc7089eb7b687bf29a3590a9
Latifa <74212974+Latifa-cyber@users.noreply.github.com>
2022-12-17 22:10:20 +0100

    Add files via upload

---
+++ lib/Welcome2.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:second_app/Welcome3.dart';
+
+
+
+class Welcome2 extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF120D3A),
+      body: Container(
+        child: Stack(
+          children: [
+            Container(
+                margin: EdgeInsets.only(top: 51,left: 75),
+
+                child:Image(image: AssetImage('Background2.png'),height:383,width: 224,)
+            ),
+            Container(
+                margin: EdgeInsets.only(top: 101,left:55.6),
+                child: Image(image: AssetImage('Welcome2.png'),height: 280.05,width: 256,)),
+            Container(
+                margin: EdgeInsets.only(top: 141,left: 0),
+                child: Image(image: AssetImage('Background2_2.png'),height: 383,width:45,)),
+            Container(
+                margin: EdgeInsets.only(top: 451,left:161),
+                child: Image(image: AssetImage('3dots2.png'),height:9,width: 39,)),
+
+            Container(
+                margin: EdgeInsets.only(top: 141,left: 311),
+                child: Image(image: AssetImage('Background2_3.png'),height: 383,width: 45,)),
+            Container(
+              alignment: Alignment.center,
+              margin: EdgeInsets.only(top: 300),
+              child: Text(
+                'Organized!',style:TextStyle(
+                  fontFamily: 'MontaguSlab',
+                  fontSize: 35,
+                  fontWeight: FontWeight.w700,
+                  color: Color(0xFF6F2DBD),
+
+              ),
+              ),
+            ),
+            Container(
+                margin: EdgeInsets.only(top:562,left: 290),
+                child: IconButton(
+                  icon:Icon(Icons.arrow_forward_ios_rounded,size: 63,
+                    color: Color(0xFFFFC727),),
+                  onPressed: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context)=>Welcome3()));
+                  },
+
+                )
+            ),
+
+
+
+
+
+
+          ],
+        ),
+      ),
+    );
+  }
+}
